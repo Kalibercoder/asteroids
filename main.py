98182327f897a7946f58a2c561f6605ba2f5836b
Theo Archer <117527935+Kalibercoder@users.noreply.github.com>
2024-10-29 12:46:28 +0100

    B: added player sprite

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player 
 from constants import * 
 def main():
     pygame.init()
@@ -6,21 +7,31 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     
+    clock = pygame.time.Clock()
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Asteroids")  # Optional: set a window title
-    
+    clock = pygame.time.Clock()
+    dt = 0
     running = True
     while running:
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:  # Allow window close
-                running = False
-        
+                running = False        
       
         screen.fill((0, 0, 0))
 
+        player.draw(screen)
+       
+
         # Refresh the display
         pygame.display.flip()
-    
+        dt = clock.tick(60) / 1000   
     pygame.quit()
 
 if __name__ == "__main__":
